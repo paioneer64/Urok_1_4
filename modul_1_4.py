1c143314db304d2d88874b489c6d6d8964320092
paioneer64 <om3095019@gmail.com>
2024-11-17 11:21:07 +0300

    Add files via upload

---
+++ modul_1_4.py
@@ -0,0 +1,9 @@
+my_string = "я изучаю программирование"
+print(len(my_string))
+name = my_string
+print(name.upper())
+print(name.lower())
+print(name.replace(" ", ""))
+example = name
+print(example[0])
+print(example[-1])
